69991b3c81bffc8567712a77e490aeb35afcf898
rahulpathak-github <rpofficial.0001@gmail.com>
2020-04-27 14:21:56 +0530

    Added button to get weather for current location

--- src/app.js
+++ src/app.js
@@ -1,91 +1,113 @@
-const express=require('express')
-const path=require('path')
-const hbs=require('hbs')
-const geocode=require('./utils/geocode')
-const forecast=require('./utils/forecast')
-const port=process.env.PORT||3000
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const { geocode, geocodeLocation } = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+const port = process.env.PORT || 3000;
 
-const app=express()
+const app = express();
 
 //define paths for express config
-const publicDirectoryPath=path.join(__dirname,'../public')
-const viewsPath=path.join(__dirname,'../templates/views')
-const partialsPath=path.join(__dirname,'../templates/partials')
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 //set up handlebars and views location
-app.set('view engine','hbs')
-app.set('views',viewsPath)
-hbs.registerPartials(partialsPath)
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 //set up static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
-app.get('',(req,res)=>{
-    res.render('index',{
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Rahul Pathak, CSE undergraduate  at IIT Bhubaneswar",
+  });
+});
 
-        title:'Weather',
-        name:'Rahul Pathak, CSE undergraduate  at IIT Bhubaneswar'
-    })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Rahul Pathak, CSE undergraduate  at IIT Bhubaneswar",
+  });
+});
 
-app.get('/about',(req,res)=>{
-    res.render('about',{
-        title:'About Me',
-        name:'Rahul Pathak, CSE undergraduate  at IIT Bhubaneswar'
-    })
-})
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    helpText: "You really need help with such a simple interface!??",
+    name: "Rahul Pathak, CSE undergraduate  at IIT Bhubaneswar",
+  });
+});
 
-app.get('/help',(req,res)=>{
-    res.render('help',{
-        title:'Help',
-        helpText:'You really need help with such a simple interface!??',
-        name:'Rahul Pathak, CSE undergraduate  at IIT Bhubaneswar'
-
-    })
-})
-
-app.get('/weather',(req,res)=>{
-    
-    if(!req.query.address){
-        return res.send({
-            error:'You must provide an address'
-        })
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address",
+    });
+  }
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          res.send({ error });
+        }
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        });
+      });
     }
-        geocode(req.query.address,(error,{latitude,longitude,location}={})=>{
-            if(error){
-                return res.send({error})
-            }
-            forecast(latitude,longitude,(error,forecastData)=>{
-                if(error){
-                    res.send({error})
-                }
-                res.send({
-                    forecast:forecastData,
-                    location,
-                    address:req.query.address 
-                })
-            })
+  );
+});
 
-        })
+app.get("/weatherFromCurrentLocation", (req, res) => {
+  const latitude = req.query.latitude;
+  const longitude = req.query.longitude;
 
+  if (!latitude && !longitude) {
+    return res.send({
+      error: "Something went wrong, refresh the page and try again",
+    });
+  }
 
-   
-})
+  geocodeLocation(latitude, longitude, (error, { location }) => {
+    if (error) {
+      return res.send(error);
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send(error);
+      }
+      res.send({
+        forecast: forecastData,
+        location: location,
+      });
+    });
+  });
+});
 
-app.get('/help/*',(req,res)=>{
-   res.render('404',{
-       title:'404 help',
-       name:'Rahul Pathak, CSE undergraduate  at IIT Bhubaneswar',
-       errorMessage:'Help article not found!'
-   })
-})
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404 help",
+    name: "Rahul Pathak, CSE undergraduate  at IIT Bhubaneswar",
+    errorMessage: "Help article not found!",
+  });
+});
 
-app.get('*',(req,res)=>{
-    res.render('404',{
-        title:'404',
-        name:'Rahul Pathak, CSE undergraduate  at IIT Bhubaneswar',
-        errorMessage:'page not found!'
-    })
-})
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Rahul Pathak, CSE undergraduate  at IIT Bhubaneswar",
+    errorMessage: "page not found!",
+  });
+});
 
-app.listen(port,()=>{
-    console.log(`server id up and running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`server id up and running on port ${port}`);
+});
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,23 +1,47 @@
-const request=require('request')
+const request = require("request");
 
+const geocode = (address, callback) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    encodeURIComponent(address) +
+    ".json?access_token=pk.eyJ1IjoicmFodWxtcGF0aGFrMSIsImEiOiJjazduaWZiZHIwYzR4M2xxcG95NXhoMGRwIn0.llRTqcUEyygMVm9INU3KKg&limit=1";
 
-const geocode=(address,callback)=>{
-    const url='https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoicmFodWxtcGF0aGFrMSIsImEiOiJjazduaWZiZHIwYzR4M2xxcG95NXhoMGRwIn0.llRTqcUEyygMVm9INU3KKg&limit=1'
-    
-    request({url,json:true},(error,{ body })=>{
-       if(error){
-          callback('Unable to connect to location services!',undefined)
-       }else if(body.features.length===0){
-          callback('Unable to find location. Try another search!',undefined)
-       }else{
-          callback(undefined,{
-             latitude:body.features[0].center[1],
-             longitude: body.features[0].center[0],
-             location: body.features[0].place_name
-          })
-       }
-    })
-    
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to location services!", undefined);
+    } else if (body.features.length === 0) {
+      callback("Unable to find location. Try another search!", undefined);
+    } else {
+      callback(undefined, {
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name,
+      });
     }
-    
-    module.exports= geocode
+  });
+};
+
+const geocodeLocation = (latitude, longitude, callback) => {
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+    longitude
+  )},${encodeURIComponent(
+    latitude
+  )}.json?access_token=pk.eyJ1IjoiaGF5YTExMSIsImEiOiJjazRoZXgya3YxNW51M25vMmJnOWlrcmo3In0.Fs5cVFjT6TOUwdlhAZyWaA&limit=1`;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to location services!", undefined);
+    } else if (body.features.length === 0) {
+      callback(
+        { error: "Unable to find location. Try another search." },
+        undefined
+      );
+    } else {
+      callback(undefined, {
+        location: body.features[0].place_name,
+      });
+    }
+  });
+};
+
+module.exports = { geocode, geocodeLocation };
